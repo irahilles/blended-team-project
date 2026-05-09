c2016da6692385d7d92deebfcd81d4e82288082c
Hyria Roman <hyrya.roman@gmail.com>
2026-05-09 11:19:01 +0300

    feat: highlight first category

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -3,13 +3,10 @@ import { refs } from './refs';
 export function renderCategories(categories) {
   const allCategories = ['all', ...categories];
   const markup = allCategories
-    .map(category =>
-      `
-  <li class="categories__item">
-   <button class="categories__btn" type="button">${category}</button>
-  </li>
-  `.trim()
-    )
+    .map((category, idx) => {
+      const buttonClass = idx === 0 ? 'categories__btn--active' : '';
+      return `<li class="categories__item"><button class="categories__btn ${buttonClass}" type="button">${category}</button></li>`;
+    })
     .join('');
   refs.categoryList.innerHTML = markup;
 }
