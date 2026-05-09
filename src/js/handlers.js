c606c5c8d44c5a89a43050bd28aa66e4700c5f70
Hyria Roman <hyrya.roman@gmail.com>
2026-05-09 12:02:13 +0300

    feat: make currently selected category active

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,14 +1,27 @@
-import { getCategories, getProducts } from "./products-api";
-import { renderCategories, renderProducts } from "./render-function";
+import { getCategories, getProducts } from './products-api';
+import { renderCategories, renderProducts } from './render-function';
+import { refs } from './refs';
 
 let currentPage = 1;
-export async function initHomePage(){
-try{
-const categories = await getCategories();
-renderCategories(categories);
-const {products} = await getProducts(currentPage);
-renderProducts(products);
-}catch(err){
+export async function initHomePage() {
+  try {
+    const categories = await getCategories();
+    renderCategories(categories);
+    const { products } = await getProducts(currentPage);
+    renderProducts(products);
+  } catch (err) {
     console.error('Error initializing home page', err);
+  }
 }
+
+export function handleCategoryClick(event) {
+  const category = event.target.textContent;
+  if (!category) return;
+
+  const button = event.target;
+  const activeButton = refs.categoryList.querySelector(
+    '.categories__btn--active'
+  );
+  activeButton.classList.toggle('categories__btn--active', false);
+  button.classList.toggle('categories__btn--active', true);
 }
